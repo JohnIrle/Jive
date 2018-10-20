c761dd3d1eac3ff54af6999076eaabeaaf5c5d99
JohnIrle <irlejohn@gmail.com>
2018-10-19 23:09:47 -0500

    Delete unused routes and models

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
   mongoURI: "mongodb://admin:admin123@ds225253.mlab.com:25253/fed-hack-a-thon",
-  secretOrKey: "secret"
+  secretOrKey: "secret",
+  facebookClientID: "738062f20c53788f581da874289b6f01",
+  facbookSecret: "bf320afb95737a9af81f9b31902146d2"
 };
