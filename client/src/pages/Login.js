cda128a882e420be33152ffb65ce26eaeaaac32d
Daniel Clayton <dclaytondev@gmail.com>
2018-10-19 23:17:53 -0500

    added css and login form

---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Logincontainer from '../components/Logincontainer';
+
+class Login extends React.Component {
+
+
+  render() {
+    return(
+      <Logincontainer />
+    );
+  }
+}
+
+export default Login;
