d2ebb2fe9c5e31b85d03cc7bc361f450f3004e93
JohnIrle <irlejohn@gmail.com>
2018-10-23 19:48:06 -0500

    Refactor home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import HomeUser from '../components/HomeUser';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   componentDidMount() {
@@ -17,7 +17,23 @@ class Home extends React.Component {
   }
 
   render() {
-    return <HomeUser />;
+    return (
+      <div className="home">
+        <div className="home-form">
+          <div className="home-form-header">
+            <h1>Make a Plan?</h1>
+          </div>
+          <div className="option">
+            <div className="option-2">
+              <Link to="/plannow">Plan now</Link>
+            </div>
+            <div className="option-3">
+              <Link to="/plantomorrow">Plan tomorrow</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
